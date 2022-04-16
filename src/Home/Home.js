b7c9dfaf3be7dff424548b407c929a98952430a7
ShakilJoy31 <joy31a@gmail.com>
2022-04-16 22:21:43 +0600

    making json file and other component is done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [animals, setAnimals] = useState(); 
+    useEffect(()=>{
+        fetch('samplePhotography.json')
+        .then(res => res.json())
+        .then(data => setAnimals(data))
+    },[]); 
+
+    console.log(animals); 
     return (
         <div>
-            <h1>This is home. </h1>
+            
         </div>
     );
 };
